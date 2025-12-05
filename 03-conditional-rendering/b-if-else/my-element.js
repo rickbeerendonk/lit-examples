932db18b1f90287576b83bde5722228438e722a5
Rick Beerendonk <rick@beerendonk.com>
2025-12-05 01:50:20 +0100

    Add "conditional rendering - if-else" example.

---
+++ 03-conditional-rendering/b-if-else/my-element.js
@@ -0,0 +1,24 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+import { LitElement, html } from 'lit';
+
+class MyElement extends LitElement {
+  static properties = {
+    show: { type: Boolean }
+  };
+
+  constructor() {
+    super();
+    this.show = true;
+  }
+
+  render() {
+    return html`
+      <button @click="${() => (this.show = !this.show)}">Toggle</button>
+      ${this.show ? html`<h1>Hello Lit!</h1>` : html`<h1>Goodbye Lit!</h1>`}
+    `;
+  }
+}
+
+customElements.define('my-element', MyElement);
