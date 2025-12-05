2f89f48df4067aaab5bfae2b69215fdb9ded15bd
Rick Beerendonk <rick@beerendonk.com>
2025-12-05 01:19:37 +0100

    Add "bootstrap" example.

---
+++ 01-bootstrap/my-element.js
@@ -0,0 +1,32 @@
+import { LitElement, html, css } from "lit";
+
+class MyElement extends LitElement {
+  static styles = css`
+    :host {
+      display: block;
+    }
+
+    .container {
+      border: 2px solid #d676ff;
+      border-radius: 10px;
+      padding: 10px;
+      background-color: #f5e6ff;
+    }
+
+    h2 {
+      color: #d676ff;
+      margin-top: 0;
+    }
+  `;
+
+  render() {
+    return html`
+      <div class="container">
+        <h2>Hello from Lit!</h2>
+        <p>This is a Lit example.</p>
+      </div>
+    `;
+  }
+}
+
+customElements.define("my-element", MyElement);
